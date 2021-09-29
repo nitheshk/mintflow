5cfdae446a9a68d256a587338c811179f3e4947b
AzureAD\NitheshK <Nitheshk@hotmail.com>
2021-09-29 14:38:12 +0530

    DOT-21: start application button add to community

---
+++ force-app/main/default/lwc/startOnlineApplication/startOnlineApplication.js
@@ -0,0 +1,30 @@
+import { LightningElement, api } from "lwc";
+import startApplication from "@salesforce/apex/ApplicationController.startApplication";
+import utils from "c/generalUtils";
+export default class StartOnlineApplication extends LightningElement {
+  @api objectApiName;
+  @api recordId;
+
+  /**
+   * @Description start application
+   * @param {*} event
+   */
+  start(event) {
+    startApplication({
+      params: {
+        recordId: this.recordId,
+        sObjectName: this.objectApiName
+      }
+    })
+      .then((result) => {
+        console.log("result:", JSON.stringify(result));
+        if (result.status === 200) {
+          window.open(result.data.url, "_blank");
+        }
+      })
+      .catch((error) => {
+        console.log("Error : " + JSON.stringify(error));
+        utils.errorMessage(this, error.body.message, "Error");
+      });
+  }
+}
