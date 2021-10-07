a6aab082fc34240fc6073daa1f96bbb0c6b8eabc
AzureAD\NitheshK <Nitheshk@hotmail.com>
2021-10-07 10:41:31 +0530

    DMFLOW-9: Fix on commit message

--- scripts/js/prepare-commit-msg.js
+++ scripts/js/prepare-commit-msg.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const message = fs.readFileSync(".git/COMMIT_EDITMSG", "utf8").trim();
-let result = message.match(/^DOT-(\d+):/g);
+let result = message.match(/^DMFLOW-(\d+):/g);
 console.log("Git Commit Message : " + message);
 if (result) {
   console.log("[POLICY] : Git Pre-Commit Message Format Passed - " + result);
@@ -8,10 +8,10 @@ if (result) {
 } else {
   console.log("[POLICY] : Your git message is not formatted correctly");
   console.log(
-    "[POLICY] : Excepted Git Commit Format -  DOT-{Ticket Number}: {Your Mmessage}"
+    "[POLICY] : Excepted Git Commit Format -  DMFLOW-{Ticket Number}: {Your Mmessage}"
   );
   console.log(
-    "[POLICY] : Example: DOT-50: Fixed typo in introduction to user guide"
+    "[POLICY] : Example: DMFLOW-50: Fixed typo in introduction to user guide"
   );
   process.exit(1);
 }
