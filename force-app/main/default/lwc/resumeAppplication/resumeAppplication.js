0e1fc7bc273527bf0a2d63f92294501c0b38b9e0
AzureAD\NitheshK <nithesh.k@digitalalign.com>
2022-01-19 15:08:55 +0530

    DMFLOW-330: lwc fixes for kyc component

--- force-app/main/default/lwc/resumeAppplication/resumeAppplication.js
+++ force-app/main/default/lwc/resumeAppplication/resumeAppplication.js
@@ -1,69 +1,131 @@
 import { LightningElement, track, api, wire } from "lwc";
 import utils from "c/generalUtils";
-import fetchApplicants from "@salesforce/apex/LwcCustomController.readApplicationWithApplicants";
-import sendEmail from "@salesforce/apex/LwcCustomController.sendResumeApplicationEmail";
+import fetchApplicants from "@salesforce/apex/LwcCustomController.readApplicationForResume";
+import sendResumeEmail from "@salesforce/apex/LwcCustomController.sendResumeApplicationEmail";
+import sendFundingEmail from "@salesforce/apex/LwcCustomController.resendFundingRequest";
 
 export default class ResumeAppplication extends LightningElement {
   @api recordId;
-  @track selectedId = [];
-  @track selectedApplicant;
+  @track application;
+
+  @track fundingFlow = false;
+  @track applicationFlow = false;
+
+  @track applicantNames;
+
+  @track selectedApplicant = [];
   @track showSpinner = false;
-  @track applicants = [];
-  @track message;
   @track disableButton = false;
+
+  /**
+   * fetch applicants
+   * @param {*} param0
+   */
   @wire(fetchApplicants, {
     applicationId: "$recordId"
   })
   fetchAllApplicants({ data, error }) {
     if (data) {
       if (data.status === 200) {
-        this.applicants = JSON.parse(data.data);
+        this.application = JSON.parse(data.data);
+        if (this.application.mflow__FlowType__c === "ApplicationFlow") {
+          this.applicationFlow = true;
+        } else if (this.application.mflow__FlowType__c === "FundingFlow") {
+          this.fundingFlow = true;
+        }
+        let applicantNames = [{ label: "All", value: "All" }];
+        let applicants = this.application.mflow__Applicants__r;
+        // eslint-disable-next-line guard-for-in
+        for (let key in applicants) {
+          applicantNames.push({
+            label: `${applicants[key].Name} (${applicants[key].RecordType.Name}) `,
+            value: applicants[key].Id
+          });
+        }
+        this.applicantNames = applicantNames;
       } else {
         console.log("error" + JSON.stringify(error));
       }
     } else if (error) {
       console.log("error : " + JSON.stringify(error));
-      utils.errorMessage(this, error.body.message, "Error fetching record");
+      utils.errorMessage(this, error.body.message, "Error");
     }
   }
+
+  /**
+   * Applicant Names Changed
+   * @param {'*'} event
+   */
   handleChange(event) {
-    let targetElement = event.target;
-    this.selectedApplicant = targetElement.options.find(
-      (opt) => opt.value === event.detail.value
-    ).label;
-    this.selectedId = [];
-    if (this.selectedApplicant === "All") {
-      this.selectedId = JSON.parse(targetElement.value);
+    this.selectedApplicant = [];
+    if (event.detail.value === "All") {
+      this.applicantNames
+        .filter((item) => {
+          return item.value !== "All";
+        })
+        .forEach((key) => {
+          this.selectedApplicant.push(key.value);
+        });
     } else {
-      this.selectedId.push(targetElement.value);
+      this.applicantNames
+        .filter((item) => {
+          return item.value === event.detail.value;
+        })
+        .forEach((key) => {
+          this.selectedApplicant.push(key.value);
+        });
     }
-    this.message = "Selected " + this.selectedApplicant;
   }
+
+  /**
+   * Send Resume Link
+   */
   sendResumeEmail() {
-    this.disableButton = true;
     if (
       utils.checkAllValidations(this.template.querySelectorAll(".validation"))
     ) {
       this.showSpinner = true;
-      sendEmail({
-        applicantIds: JSON.stringify(this.selectedId)
+      this.disableButton = true;
+      sendResumeEmail({
+        request: {
+          data: JSON.stringify(this.selectedApplicant)
+        }
       })
         .then((result) => {
           if (result.status === 200) {
-            this.disableButton = false;
-            this.message = "Email sent to " + this.selectedApplicant;
-            utils.successMessage(this, "Email sent");
+            utils.successMessage(this, "Email sent to Applicant", "Success");
           }
+          this.disableButton = false;
         })
         .catch((error) => {
           this.disableButton = false;
           console.log("error : " + JSON.stringify(error));
-          utils.errorMessage(
-            this,
-            error.body.message,
-            "Error on sending email"
-          );
+          utils.errorMessage(this, error.body.message, "Error");
         });
     }
+  }
+
+  /**
+   * sendFundingEmail
+   */
+  sendFundingEmail() {
+    this.showSpinner = true;
+    this.disableButton = true;
+    sendFundingEmail({
+      request: {
+        data: this.recordId
+      }
+    })
+      .then((result) => {
+        this.disableButton = false;
+        if (result.status === 200) {
+          utils.successMessage(this, "Email sent to Succesfully", "Success");
+        }
+      })
+      .catch((error) => {
+        this.disableButton = false;
+        console.log("error : " + JSON.stringify(error));
+        utils.errorMessage(this, error.body.message, "Error");
+      });
   }
 }
