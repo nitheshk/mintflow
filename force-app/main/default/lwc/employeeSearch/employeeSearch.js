108a73bd5008626ef4c4a8a9f01e9688ec3c12f7
Lakshmi <lakshmi@digitalalign.com>
2022-02-02 17:23:33 +0530

    DMFLOW-397:employee search

--- force-app/main/default/lwc/employeeSearch/employeeSearch.js
+++ force-app/main/default/lwc/employeeSearch/employeeSearch.js
@@ -1,19 +1,33 @@
-import { track, api } from "lwc";
-import constants from "c/validationFormatUtils";
-import serachEmployees from "@salesforce/apex/LWCFinancialInstituteSiteController.retriveEmployees";
+import { LightningElement, track, api } from "lwc";
+
+import searchEmployees from "@salesforce/apex/LWCFinancialInstituteSiteController.retriveEmployees";
+import { NavigationMixin } from "lightning/navigation";
 import utils from "c/generalUtils";
 const columns = [
   { label: "Employee Id", fieldName: "mflow__EmployeeId__c" },
   { label: "FirstName", fieldName: "FirstName" },
   { label: "LastName", fieldName: "LastName" },
   { label: "Mobile Number", fieldName: "MobilePhone" },
-  { label: "Email", fieldName: "Email", type: "Email" }
+  { label: "Email", fieldName: "Email", type: "Email" },
+  {
+    type: "button",
+    typeAttributes: {
+      label: "View",
+      name: "View",
+      title: "View",
+      disabled: false,
+      value: "view",
+      iconPosition: "left"
+    }
+  }
 ];
 
-export default class EmployeeSearch extends constants {
-  columns = columns;
+export default class EmployeeSearch extends NavigationMixin(LightningElement) {
+  @track columnsdata;
+  columnsdata = columns;
   @track searchData;
   @track searchFilter = {};
+
   searchString = "";
   showSpinner = false;
   @track openModal = false;
@@ -25,7 +39,7 @@ export default class EmployeeSearch extends constants {
 
   handleSearch() {
     this.showSpinner = true;
-    serachEmployees({
+    searchEmployees({
       params: {
         searchString: this.searchString,
         searchFilter: this.searchFilter,
@@ -33,8 +47,8 @@ export default class EmployeeSearch extends constants {
       }
     })
       .then((result) => {
-        console.log(result);
-        this.searchData = result.data;
+        this.searchData = JSON.parse(result.data);
+        console.log("this.searchData::" + JSON.stringify(this.searchData));
         this.showSpinner = false;
         console.log(this.employeeContactType);
       })
@@ -47,7 +61,32 @@ export default class EmployeeSearch extends constants {
         this.showSpinner = false;
       });
   }
+  callRowAction(event) {
+    const recId = event.detail.row.Id;
+    const actionName = event.detail.action.name;
+    console.log("actionName" + actionName);
+    console.log("recId" + recId);
 
+    if (actionName === "Edit") {
+      this[NavigationMixin.Navigate]({
+        type: "standard__recordPage",
+        attributes: {
+          recordId: recId,
+          objectApiName: "Account",
+          actionName: "edit"
+        }
+      });
+    } else if (actionName === "View") {
+      this[NavigationMixin.Navigate]({
+        type: "standard__recordPage",
+        attributes: {
+          recordId: recId,
+          objectApiName: "Account",
+          actionName: "view"
+        }
+      });
+    }
+  }
   handleChange(event) {
     let targetElement = event.target;
     this.searchFilter[targetElement.dataset.fieldname] = targetElement.value;
