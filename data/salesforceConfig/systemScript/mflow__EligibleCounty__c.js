a7ab7d820fa9b16bba6d24735711203a9381dd90
Kshama <kshama.v@digitalalign.com>
2022-02-08 15:10:48 +0530

    DMFLOW-482: County config

---
+++ data/salesforceConfig/systemScript/mflow__EligibleCounty__c.js
@@ -0,0 +1,30 @@
+const utils = require("../utils.js");
+let configPath = "data/salesforceConfig/systemConfig/";
+let scriptPath = "data/salesforceConfig/systemScript/";
+let objectName = "mflow__EligibleCounty__c";
+
+let scriptToRun = `sfdx force:apex:execute  -f ${scriptPath}${objectName}.apex  --json `;
+utils
+  .runCommand(scriptToRun)
+  .then((result) => {
+    const data = utils.fetchRecords(result);
+
+    // <==  update change
+    data.forEach(function (item, index) {
+      utils.replaceUnwantedFields(item);
+      delete item.Name;
+      item.attributes.referenceId = "EligibleCountyRef_" + index;
+    });
+    //   update change ==>
+    utils
+      .createFile(
+        `${configPath}${objectName}.json`,
+        JSON.stringify({ records: data })
+      )
+      .catch((err) => {
+        console.log("errr :" + JSON.stringify(err));
+      });
+  })
+  .catch((err) => {
+    console.log("err :" + err);
+  });
