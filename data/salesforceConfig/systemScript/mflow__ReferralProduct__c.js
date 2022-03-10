55cd6e71ebf08534b972709eee9a5d4bc72a83a8
AzureAD\NitheshK <nithesh.k@digitalalign.com>
2022-03-10 14:40:35 +0530

    DAT-637: Addtional Product  Configuration Update

--- data/salesforceConfig/systemScript/mflow__ReferralProduct__c.js
+++ data/salesforceConfig/systemScript/mflow__ReferralProduct__c.js
@@ -1,7 +1,7 @@
 const utils = require("../utils.js");
 let configPath = "data/salesforceConfig/systemConfig/";
 let scriptPath = "data/salesforceConfig/systemScript/";
-let objectName = "mflow__AdditionalProduct__c";
+let objectName = "mflow__ReferralProduct__c";
 
 let scriptToRun = `sfdx force:apex:execute  -f ${scriptPath}${objectName}.apex  --json `;
 utils
@@ -11,27 +11,20 @@ utils
     // <== Script to update change for each config
     data.forEach(function (item, index) {
       utils.replaceUnwantedFields(item);
-      item.attributes.referenceId = "AdditionalProductRef" + index;
+      item.attributes.referenceId = "ReferralProductRef" + index;
       if (item.mflow__FinancialProduct__c) {
-        item.mflow__FinancialProduct__c =
-          "@" + item.mflow__FinancialProduct__r.mflow__InternalCode__c;
+        item.mflow__FinancialProduct__c = "@" + item.mflow__FinancialProduct__r.mflow__InternalCode__c;
         delete item.mflow__FinancialProduct__r;
       }
       if (item.mflow__CrossSellProduct__c) {
-        item.mflow__CrossSellProduct__c =
-          "@" + item.mflow__CrossSellProduct__r.mflow__InternalCode__c;
+        item.mflow__CrossSellProduct__c = "@" + item.mflow__CrossSellProduct__r.mflow__InternalCode__c;
         delete item.mflow__CrossSellProduct__r;
       }
     });
     //   Script to update change for each config ==>
-    utils
-      .createFile(
-        `${configPath}${objectName}.json`,
-        JSON.stringify({ records: data })
-      )
-      .catch((err) => {
-        console.log("errr :" + JSON.stringify(err));
-      });
+    utils.createFile(`${configPath}${objectName}.json`, JSON.stringify({ records: data })).catch((err) => {
+      console.log("errr :" + JSON.stringify(err));
+    });
   })
   .catch((err) => {
     console.log("err :" + err);
