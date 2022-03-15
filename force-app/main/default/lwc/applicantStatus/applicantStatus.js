21b786783570f872b140adab679324d4616fa40f
Lakshmi <lakshmi@digitalalign.com>
2022-03-15 21:58:09 +0530

    DAT-864: lwc styling for status and reports

--- force-app/main/default/lwc/applicantStatus/applicantStatus.js
+++ force-app/main/default/lwc/applicantStatus/applicantStatus.js
@@ -5,10 +5,15 @@ export default class ApplicantStatus extends LightningElement {
   @api sObjectName;
   @api titleName;
   identityVerified = false;
+  kycStatus = false;
 
   renderedCallback() {
     if (this.record.mflow__IsPhoneNumberVerified__c != "Failed") {
       this.identityVerified = true;
+    }
+    console.log("this.record.mflow__KYCStatus__c::" + this.record.mflow__KYCStatus__c);
+    if (this.record.mflow__KYCStatus__c === "Passed") {
+      this.kycStatus = true;
     }
   }
 }
