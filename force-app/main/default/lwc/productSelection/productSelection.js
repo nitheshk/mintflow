f1c821426d7d279b08cbe6bc0f8c634188c3bbf1
AzureAD\NitheshK <nithesh.k@digitalalign.com>
2022-03-22 19:53:47 +0530

    DAT-1045: Create Product Cart Using LWC

--- force-app/main/default/lwc/productSelection/productSelection.js
+++ force-app/main/default/lwc/productSelection/productSelection.js
@@ -3,14 +3,46 @@ import utils from "c/generalUtils";
 import getProducts from "@salesforce/apex/LwcOnlineSiteController.productSelector";
 
 export default class ProductSelection extends LightningElement {
-  @track productList;
+  allProducts;
+  @track productsToShow;
+  @track productMap = new Map();
+  @track selectedProductType = "All";
+  @track productTypes = [];
   @wire(getProducts, { request: {} })
   products({ data, error }) {
-    console.log("data::" + JSON.stringify(data));
     if (data?.status === 200) {
-      this.productList = JSON.parse(data.data);
+      this.allProducts = JSON.parse(data.data);
+      this.productsToShow = JSON.parse(data.data);
+      this.productTypes.push("All");
+      self = this;
+      this.allProducts?.forEach(function (product) {
+        if (!self.productTypes.includes(product.mflow__Category__c)) {
+          self.productTypes.push(product.mflow__Category__c);
+        }
+      });
     } else if (error) {
       console.log("error : " + JSON.stringify(error));
     }
+  }
+
+  /**
+   *
+   * @param {*} event
+   * @returns
+   */
+  handleProductTypeChange(event) {
+    let productType = event.target.dataset.type;
+    if (productType === this.selectedProductType) {
+      return;
+    }
+
+    if (productType == "All") {
+      this.productsToShow = this.allProducts;
+    } else {
+      this.productsToShow = this.allProducts?.filter((product) => {
+        return product.mflow__Category__c === productType;
+      });
+    }
+    this.selectedProductType = productType;
   }
 }
