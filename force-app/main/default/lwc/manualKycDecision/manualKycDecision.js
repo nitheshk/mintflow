3f6e5e0b25bad3c6467f9211266104d2cede3bc5
AzureAD\NitheshK <nithesh.k@digitalalign.com>
2022-04-03 16:37:40 +0530

    DMFLOW-613: Resubmit Application if KYC failed

--- force-app/main/default/lwc/manualKycDecision/manualKycDecision.js
+++ force-app/main/default/lwc/manualKycDecision/manualKycDecision.js
@@ -2,6 +2,7 @@ import { LightningElement, api, wire, track } from "lwc";
 import utils from "c/generalUtils";
 import getPickListValues from "@salesforce/apex/LwcCustomController.fetchPickListValues";
 import updateKycDecision from "@salesforce/apex/LwcCustomController.updateKycDecision";
+import submitToCoreSystem from "@salesforce/apex/ApplicationController.submitToCoreSystem";
 export default class ManualKycDecision extends LightningElement {
   @api objectApiName;
   @api recordId;
@@ -11,16 +12,6 @@ export default class ManualKycDecision extends LightningElement {
   @track showApplicationTab = false;
   @track showApplicantTab = false;
   @track showSpinner = false;
-
-  get approvalTypes() {
-    return [
-      { label: "Application", value: "Application" },
-      {
-        label: "Application And Applicants",
-        value: "Application And Applicants"
-      }
-    ];
-  }
 
   /**
    * Load Picklist value for components
@@ -39,7 +30,7 @@ export default class ManualKycDecision extends LightningElement {
           (item) => item.value === "Passed" || item.value === "Failed"
         );
 
-        this.applicantStatus = JSON.parse(data.data).mflow__Applicant__c.mflow__KYCStatus__c.filter(
+        this.applicantStatus = JSON.parse(data.data)?.mflow__Applicant__c.mflow__KYCStatus__c.filter(
           (item) => item.value === "Passed" || item.value === "Failed"
         );
         if (this.objectApiName === "mflow__Application__c") {
@@ -76,30 +67,95 @@ export default class ManualKycDecision extends LightningElement {
    */
   updateStatus() {
     if (utils.checkAllValidations(this.template.querySelectorAll(".validation"))) {
-      this.showSpinner = true;
-      updateKycDecision({
-        params: {
-          recordId: this.recordId,
-          objectApiName: this.objectApiName,
-          status: this.collectedInfo.status,
-          approvalType: this.collectedInfo.approvalType
-        }
-      })
-        .then((result) => {
-          console.log("result" + JSON.stringify(result));
-          if (result.status === 200) {
-            utils.successMessage(this, "Status Updated", "Success");
+      if (this.objectApiName === "mflow__Application__c") {
+        return this.updateApplicationKyc();
+      } else if (this.objectApiName === "mflow__Applicant__c") {
+        return this.updateApplicantKyc();
+      }
+    }
+  }
+
+  /**
+   * updateApplicationKyc
+   */
+  updateApplicationKyc() {
+    this.showSpinner = true;
+    updateKycDecision({
+      params: {
+        recordId: this.recordId,
+        objectApiName: this.objectApiName,
+        status: this.collectedInfo.status
+      }
+    })
+      .then((result) => {
+        console.log("result" + JSON.stringify(result));
+        if (result.status === 200) {
+          let applicationData = JSON.parse(result.data);
+          utils.successMessage(this, "Kyc Status updated", "Success");
+          if (applicationData.mflow__Status__c === "Submitted") {
+            submitToCoreSystem({
+              request: {
+                applicationId: this.recordId
+              }
+            })
+              .then((result2) => {
+                if (result2.status === 200) {
+                  utils.successMessage(this, "Application Submitted", "Success");
+                } else {
+                  utils.errorMessage(this, result2.data, "Error");
+                }
+                this.showSpinner = false;
+                utils.refreshLwcPage();
+              })
+              .catch((error2) => {
+                console.log("error : " + JSON.stringify(error2));
+                this.showSpinner = false;
+                utils.refreshLwcPage();
+                utils.errorMessage(this, "Something went Wrong", "Error");
+              });
           } else {
-            utils.errorMessage(this, result.data, "Error");
+            this.showSpinner = false;
+            utils.refreshLwcPage();
           }
+        } else {
+          utils.errorMessage(this, result.data, "Error");
           this.showSpinner = false;
           utils.refreshLwcPage();
-        })
-        .catch((error) => {
-          console.log("error : " + JSON.stringify(error));
-          this.showSpinner = false;
-          utils.errorMessage(this, error.body.message, "Error");
-        });
-    }
+        }
+      })
+      .catch((error) => {
+        console.log("error : " + JSON.stringify(error));
+        this.showSpinner = false;
+        utils.errorMessage(this, "Something went Wrong", "Error");
+      });
+  }
+
+  /**
+   * updateApplicantKyc
+   */
+  updateApplicantKyc() {
+    this.showSpinner = true;
+    updateKycDecision({
+      params: {
+        recordId: this.recordId,
+        objectApiName: this.objectApiName,
+        status: this.collectedInfo.status
+      }
+    })
+      .then((result) => {
+        console.log("result" + JSON.stringify(result));
+        if (result.status === 200) {
+          utils.successMessage(this, "Status Updated", "Success");
+        } else {
+          utils.errorMessage(this, result.data, "Error");
+        }
+        this.showSpinner = false;
+        utils.refreshLwcPage();
+      })
+      .catch((error) => {
+        console.log("error : " + JSON.stringify(error));
+        this.showSpinner = false;
+        utils.errorMessage(this, "Something went Wrong", "Error");
+      });
   }
 }
