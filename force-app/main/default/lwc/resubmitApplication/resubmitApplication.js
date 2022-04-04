4c04994ebffab4c262f31fc54a591932a18d7a0d
AzureAD\NitheshK <nithesh.k@digitalalign.com>
2022-04-04 14:10:01 +0530

    DMFLOW-613: Resubmit Application if KYC failed

--- force-app/main/default/lwc/resubmitApplication/resubmitApplication.js
+++ force-app/main/default/lwc/resubmitApplication/resubmitApplication.js
@@ -1,28 +1,58 @@
 import { LightningElement, track, api, wire } from "lwc";
 import utils from "c/generalUtils";
 import resubmitApplication from "@salesforce/apex/LwcCustomController.resubmitApplication";
-
+import lwcRecordLevelVisibility from "@salesforce/apex/LwcCustomController.lwcRecordLevelVisibility";
+import { getRecord } from "lightning/uiRecordApi";
 export default class ResubmitApplication extends LightningElement {
-  @track showSpinner = false;
-  @track disableButton = false;
+  @api objectApiName;
   @api recordId;
+  @api filter;
+  @track showSpinner = false;
+  @track record;
+  /**
+   * refresh the current record data
+   * @param {*} param0
+   */
+  @wire(getRecord, { recordId: "$recordId", fields: ["$objectApiName" + ".Id"] })
+  getCurrentRecord({ data }) {
+    if (data) {
+      lwcRecordLevelVisibility({
+        params: {
+          recordId: this.recordId,
+          objectApiName: this.objectApiName,
+          filter: this.filter
+        }
+      }).then((result) => {
+        if (result.status === 200) {
+          this.record = JSON.parse(result.data);
+        } else {
+          this.record = null;
+        }
+      });
+    }
+  }
+
+  /**
+   * Resubmit
+   */
   Resubmit() {
     this.showSpinner = true;
-    this.disableButton = true;
     resubmitApplication({
       applicationId: this.recordId
     })
       .then((result) => {
         if (result.status === 200) {
           utils.successMessage(this, "Application added to queue");
+        } else {
+          utils.errorMessage(this, result.data, "Error");
         }
-        this.disableButton = false;
+        this.showSpinner = false;
         utils.refreshLwcPage();
       })
       .catch((error) => {
-        this.disableButton = false;
+        this.showSpinner = false;
         console.log("error : " + JSON.stringify(error));
-        utils.errorMessage(this, error.body.message, "Error");
+        utils.errorMessage(this, "Something went Wrong", "Error");
       });
   }
 }
