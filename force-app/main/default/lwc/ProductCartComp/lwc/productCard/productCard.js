3d19e7410d4b5953312ef336b21a64bf3307f883
AzureAD\NitheshK <nithesh.k@digitalalign.com>
2022-04-06 00:55:12 +0530

    DMFLOW-666: Product Feature Configuration for CD products

--- force-app/main/default/lwc/ProductCartComp/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/ProductCartComp/lwc/productCard/productCard.js
@@ -2,19 +2,12 @@ import { LightningElement, api, track } from "lwc";
 import { customDispatchEvent } from "c/generalUtils";
 export default class ProductCard extends LightningElement {
   @api product;
-  @track logoUrl;
-  connectedCallback() {
-    this.logoUrl = "https://ik.imagekit.io/disczlxo8kv/P1FCU/" + this.product.mflow__ImageURL__c;
-    console.log("logo " + this.logoUrl);
-  }
 
   /**
-   *
    * @param {*} event
    */
   handleProductSelectChange(event) {
     let productCode = event.target.dataset.productcode;
-    console.log("child productCode:", productCode);
     customDispatchEvent(this, "productselection", { productCode: productCode });
   }
 }
