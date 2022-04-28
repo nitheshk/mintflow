a59872e580f251c3b66f6dc73fc1429ec01c4ee0
Lakshmi <lakshmi@digitalalign.com>
2022-04-28 14:10:00 +0530

    DMFLOW-737:oow status

--- force-app/main/default/lwc/applicantStatus/applicantStatus.js
+++ force-app/main/default/lwc/applicantStatus/applicantStatus.js
@@ -65,4 +65,7 @@ export default class ApplicantStatus extends NavigationMixin(LightningElement) {
         utils.errorMessage(this, error.body.message, "Error");
       });
   }
+  handleNavigate() {
+    this.openModal = true;
+  }
 }
