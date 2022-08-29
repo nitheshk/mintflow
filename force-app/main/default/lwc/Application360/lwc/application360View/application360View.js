16ebe25d6fd8dd60af82130528b9118f2d9c19bc
Lakshmi Parameswarane <lakshmi@digitalalign.com>
2022-08-29 20:57:15 +0530

    PODSIX-179: asset and liability page

--- force-app/main/default/lwc/Application360/lwc/application360View/application360View.js
+++ force-app/main/default/lwc/Application360/lwc/application360View/application360View.js
@@ -25,6 +25,16 @@ export default class Application360View extends LightningElement {
       //console.log("result.data   : " + result.data);
       if (result.data.status === 200) {
         this.record = JSON.parse(result.data.data);
+        this.record.mflow__Applicants__r.forEach((applicant) => {
+          console.log(JSON.stringify("json" + applicant.mflow__AssetAndLiabilities__r));
+          applicant.mflow__Assets__r = applicant.mflow__AssetAndLiabilities__r.filter(function (AssetorLiability) {
+            return AssetorLiability.mflow__Type__c == "Asset";
+          });
+          applicant.mflow__Liabilities__r = applicant.mflow__AssetAndLiabilities__r.filter(function (AssetorLiability) {
+            return AssetorLiability.mflow__Type__c == "Liability";
+          });
+        });
+        applicant.mflow__AssetAndLiabilities__r = null;
         //console.log("record ::: " + JSON.stringify(this.record));
       }
     } else if (result.error) {
